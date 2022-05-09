1dad0d2108e5b434818e0c28303b8e2640d3334a
Paul Nadal <paul.nadal@epfl.ch>
2022-05-09 17:59:31 +0200

    add graph conversion to main

--- main.py
+++ main.py
@@ -9,16 +9,18 @@ def run():
 
     board = chess.Board()
     player = False
-    move = None
+    move_engine = "a0a0"
+    move_player = "a0a0"
 
     while not board.is_game_over():
         if not player:
             result = engine.play(board, chess.engine.Limit(time=0.1))
             board.push(result.move)
             #call arduino
-            print(get_path(move[2:] + result.move))
+            print(get_path(move_engine[2:] + result.move))
             #call arduino
             print(get_path(result.move))
+            move_engine = result.move
             print("Computer has played : " + str(result.move))
             with open("board.txt", "w") as f:
                 f.write(str(board))
@@ -27,8 +29,8 @@ def run():
             input("Press enter when you have played!")
             with open("board.txt", "r") as f:
                 played_board = "".join(f.readlines())
-            move = diff(board, played_board)
-            result_move = chess.Move.from_uci(str(move))
+            move_player = diff(board, played_board)
+            result_move = chess.Move.from_uci(str(move_player))
             board.push(result_move)
             print("You have played : " + str(result_move))
             player = False
