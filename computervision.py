a2ee3be15eb0fc346c850eada07a5aea937b268a
jules20022000 <jules.maglione@epfl.ch>
2022-05-19 22:19:02 +0200

    good version CV

--- computervision.py
+++ computervision.py
@@ -6,36 +6,39 @@ def get_move_from_player():
     cropXBegin = 190
     cropXEnd = 320
     cropYTop = 50
-    cropYBottom = 70
+    cropYBottom = 65 
 
-    img = cv2.imread("chess.jpg") 
+    img = cv2.imread("./chess.jpg") 
 
-    lower_green = np.array([55,40,40])
-    upper_green = np.array([68,255,255])
+    lower_green = np.array([36, 60, 60])
+    upper_green = np.array([86, 255, 255])
 
-    lower_red = np.array([0,40,40])
-    upper_red = np.array([1,255,255])
+    lower_red = np.array([0, 100, 100])
+    upper_red = np.array([10, 255, 255])
 
     res = ""
-    board = img[cropYTop:img.shape[0] - cropYBottom , cropXBegin:img.shape[1] - cropXEnd, :]
-    board = cv2.rotate(board, cv2.ROTATE_180)
+    board = img[cropYTop:img.shape[0] - cropYBottom, cropXBegin:img.shape[1] - cropXEnd, :]
     cv2.namedWindow("output", cv2.WINDOW_NORMAL)
     cv2.imshow("output", board)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     for x in range(0, board.shape[0] - 8, board.shape[0]//8):
         for y in range(0, board.shape[1] - 8, board.shape[1]//8):      
-            square = board[x : x + board.shape[0]//8, y : y + board.shape[1]//8, :]   
+            square = board[x : x + board.shape[0]//8, y : y + board.shape[1]//8, :]    
+            """cv2.imshow("output", square)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()"""
             hsv = cv2.cvtColor(square, cv2.COLOR_BGR2HSV)
 
-            green_mask = cv2.inRange(hsv, lower_green, upper_green)
-            red_mask = cv2.inRange(hsv, lower_red, upper_red)
+            mask_green =  cv2.inRange(hsv, lower_green, upper_green)
+            hasGreen = np.sum(mask_green)
 
-            hasRed = np.sum(red_mask)
-            hasGreen = np.sum(green_mask) 
+            mask_red =  cv2.inRange(hsv, lower_red, upper_red)
+            hasRed = np.sum(mask_red)
 
+            print("red : ", hasRed)
 
-            if hasGreen > 0 : 
+            if hasGreen > 15000 or hasRed > 500:
                 res += "x" 
             else:
                 res += '.'
