f0de81a62d24bd2a06548fa77423e457005c5467
Khalil Acheche <khalil.acheche@epfl.ch>
2023-01-21 23:54:47 +0100

    upload metadata

--- Main Firmware/test_cv.py
+++
@@ -1,8 +0,0 @@
-
-from ComputerVision import ComputerVision
-
-for i in range(3):
-    ComputerVision.capture()
-played_board = ComputerVision.process_image()
-for i in range(8):
-    print(played_board[i,i+8])
